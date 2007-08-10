3a4607da7dbde8bfd1db8ce394515eea6cf5004d
cyu <cyu@db318f2f-d136-0410-a6fa-a44e35a74f1a>
2007-08-10 01:52:18 +0000

    update for deployment to vendor/beast
    git-svn-id: http://svn.codeeg.com/beast@10 db318f2f-d136-0410-a6fa-a44e35a74f1a

--- install.rb
+++
@@ -1,7 +0,0 @@
-vendor_dir = File.join(RAILS_ROOT, 'vendor')
-
-default_views_dir = File.join(vendor_dir, 'plugins', 'private_message', 'views')
-target_dir = File.join(vendor_dir, 'beast', 'private_message')
-
-FileUtils.mkdir_p target_dir
-FileUtils.cp_r default_views_dir, target_dir
