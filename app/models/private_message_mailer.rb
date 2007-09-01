06e7e5b9b6ad660d6549443198b340f1f079e704
cyu <cyu@db318f2f-d136-0410-a6fa-a44e35a74f1a>
2007-09-01 15:54:07 +0000

    allow mailer sender to be configured
    git-svn-id: http://svn.codeeg.com/beast@22 db318f2f-d136-0410-a6fa-a44e35a74f1a

--- app/models/private_message_mailer.rb
+++ app/models/private_message_mailer.rb
@@ -3,7 +3,7 @@ class PrivateMessageMailer < ActionMailer::Base
     @subject    = "[Beast] You\'ve received a private message from {sender}"[:beast_pm_email_subject, sender.display_name]
     @body       = {:sender => sender, :recipient => recipient, :domain => domain, :messages_url => messages_url}
     @recipients = recipient.email
-    @from       = 'beast@' + domain.split(":").first
+    @from       = Object.const_defined?(:MAILER_DEFAULT_SENDER) ? MAILER_DEFAULT_SENDER : 'beast@' + domain.split(":").first
     @sent_on    = sent_at
     @headers    = {}
   end
